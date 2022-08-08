299192d27f116a7f0a2eb4917b2117af79c689a6
MD <dhmedy@googlemail.com>
2022-08-08 13:03:50 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let array = [
+    "imageTeilnehmer000supercode.jpg",
+    "imageTeilnehmer001supercode.jpg",
+    "imageTeilnehmer002supercode.jpg",
+    "imageTeilnehmer003supercode.jpg",
+    "imageTeilnehmer004supercode.jpg",
+    "imageTeilnehmer005supercode.jpg",
+    "imageTeilnehmer006supercode.jpg",
+    "imageTeilnehmer007supercode.jpg",
+    "imageTeilnehmer008supercode.jpg",
+    "imageTeilnehmer009supercode.jpg",
+    "imageTeilnehmer010supercode.jpg",
+    "imageTeilnehmer011supercode.jpg",
+    "imageTeilnehmer012supercode.jpg",
+    "imageTeilnehmer013supercode.jpg",
+    "imageTeilnehmer014supercode.jpg",
+    "imageTeilnehmer015supercode.jpg",
+    "imageTeilnehmer016supercode.jpg",
+    "imageTeilnehmer017supercode.jpg",
+    "imageTeilnehmer018supercode.jpg",
+    "imageTeilnehmer019supercode.jpg"
+];
+
+// Kopiere die Bilder 007-014 und deklariere sie als Variable copyImg1.
+let copyImg1 = array.slice(7, 15);
+console.log(copyImg1);
+
+// Kopiere die Bilder 006-011 und deklariere sie als Variable copyImg2.
+let copyImg2 = array.slice(6, 12);
+console.log(copyImg2);
